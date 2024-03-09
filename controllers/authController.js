51be0bf8609daa5d54a3900b7e097ef110db7dab
Avi Mukesh <avimukesh10@gmail.com>
2024-03-09 14:21:06 +0000

    login error messages changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,10 +14,16 @@ const login = asyncHandler(async (req, res) => {
   }
 
   const foundUser = await User.findOne({ username }).exec();
-  if (!foundUser) return res.status(401).json({ message: "Unauthorized" });
+  if (!foundUser)
+    return res
+      .status(401)
+      .json({ message: "Invalid details. Please try again." });
 
   const match = await bcrypt.compare(password, foundUser.password);
-  if (!match) return res.status(401).json({ message: "Unauthorized" });
+  if (!match)
+    return res
+      .status(401)
+      .json({ message: "Invalid details. Please try again." });
 
   const accessToken = jwt.sign({ username }, process.env.ACCESS_TOKEN_SECRET, {
     expiresIn: "60m",
