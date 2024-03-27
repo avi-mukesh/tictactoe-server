bf575e283b6114f45c55ef36de9058814a5a99eb
Avi Mukesh <avimukesh10@gmail.com>
2024-03-27 15:07:51 +0000

    secured the /user routes

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -7,13 +7,22 @@ const verifyJWT = (req, res, next) => {
     return res.status(401).json({ message: "Unauthorized" });
   }
 
-  const token = authHeader.split("")[1];
+  const token = authHeader.split(" ")[1];
+  console.log(token);
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) return res.status(403).json({ message: "Forbidden" });
-    req.user = decoded.UserInfo.username;
-    next();
-  });
+  jwt.verify(
+    token,
+    process.env.ACCESS_TOKEN_SECRET,
+    (
+      err
+      // , decoded
+    ) => {
+      if (err) return res.status(403).json({ message: "Forbidden" });
+      // console.log(decoded);
+      // req.username = decoded.username;
+      next();
+    }
+  );
 };
 
 module.exports = verifyJWT;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const usersController = require("../controllers/userController");
 const verifyJWT = require("../middleware/verifyJWT");
 
-// router.use(verifyJWT);
+router.use(verifyJWT);
 router.route("/").get(usersController.getUsers);
 router.route("/:id").get(usersController.getUser);
 router.route("/:id").put(usersController.updateUserPassword);
